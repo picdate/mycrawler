f0aa3e68f0da40a54a0ce6d35ca0bddab0c79588
picdate <1729373298@qq.com>
2023-02-20 22:27:54 +0800

    增加爬取页范围选择和打包

--- test.py
+++ test.py
@@ -4,6 +4,7 @@ from sqlconnector import MysqlConnector
 from bean import Game
 from bs4 import BeautifulSoup
 import time
+import random
 
 def getDownloadUrl(str):
     pattern=re.compile('(https:\/\/pan\.baidu\.com\/s\/)[\w]+(\?)(pwd=)[\w]{4}')
@@ -15,76 +16,108 @@ def getfourURL(str):
     search_res=pattern.search(str)
     return search_res
 
+def getRegResualt(str,expression):
+    pattern=re.compile(expression)
+    res=pattern.search(str)
+    if res==None :return res
+    else :return res.group()
 
-index=6
-while index<=10:
-    myproxies={'http':requests.get(url="http://localhost:5555/random").content.decode()}
-    print("开始爬取第"+index.__str__()+"页内容")
-    x520_url="https://xxxxx520.com/switchyouxi/page/"+index.__str__()
-    pro_headers={"User-Agent":"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.22 Safari/537.36 SE 2.X MetaSr 1.0"}
-    html= requests.get(url=x520_url,headers=pro_headers,proxies=myproxies)
-    soup=BeautifulSoup(html.text,"lxml")
-    res=soup.select("div .placeholder")
 
-    for item in res:
-        tag_a=item.find("a")
-        href=tag_a.attrs['href']
-        inner_html=requests.get(url=href,headers=pro_headers,proxies=myproxies)
-        inner_page=BeautifulSoup(inner_html.text,"lxml")
-        """获取头部"""
-        header=inner_page.find_all("header",class_="entry-header")[0]
-        """获取描述体"""
-        descript_body=inner_page.find_all("div",class_="entry-wrapper")[0]
-        """获取名称，平台,语言属性"""
-        game_name=header.contents[3].text
-        categroy=header.contents[1].contents[3]
-        platform="无"
-        language="无"
-        flag=0;
-        for i in categroy.contents:
-            if i!="\n" and i!="":
-                if flag==0:
-                    platform=i.text.strip()
-                    flag=1
-                    continue
-                if flag==1:
-                    language=i.text.strip()
-                    break
+def xxxswitch_crawler():
 
-                
-
-        """获取描述"""
-        descript=descript_body.contents[3].text
-
-        """获取下载连接属性"""
-        pattern = re.compile('/[0-9]+')
-        number=pattern.search(href)
-        r_number=number.group()[1:]
-        download_pag_url="https://xxxxx520.com/go/?post_id="+r_number
-        """可能是script结构，也有可能直接是百度链接"""
-        download_pag_script=requests.get(download_pag_url,headers=pro_headers,proxies=myproxies).content.decode()
-        
-        search_res=getDownloadUrl(download_pag_script)
-        if(search_res!=None):
-            download=search_res.group()
-        else :
-            search_res=getfourURL(download_pag_script)
-            """没有链接"""
-            if(search_res==None):
+    print("-----输入爬取范围-----  (?,?)")
+    range_str=input()
+    if(getRegResualt(range_str,"^\([\d]+,[\d]+\)$")==None):
+        print("范围格式错误") 
+        return None
+    else:
+       left=""
+       right=""
+       temp=""
+       for i in range(0,range_str.__len__()):
+            if(range_str[i]=="("):
                 continue
-            download_url=search_res.group()
-            """script结构处理"""
-            download_html=requests.get(download_url,headers=pro_headers,proxies=myproxies)
-            download_pag_soup=BeautifulSoup(download_html.text,"lxml")
-            download=download_pag_soup.find("div",class_="entry-content u-text-format u-clearfix").text
-        
-        
+            if(range_str[i]==","):
+                left=int(temp)
+                temp=""
+                continue
+            if(ord(range_str[i])<=57 and ord(range_str[i])>=48):
+                temp=temp+range_str[i].__str__()
+       right=int(temp)
+    
 
-        temp_game=Game(name=game_name,language=language,description=descript,download=download,platefrom=platform)
-        insert_sql="INSERT INTO game  (game.`name`,game.`language`,description,download,plateform) SELECT  '"+game_name+"','"+language+"','"+descript+"','"+download+"','"+platform+"' FROM DUAL   WHERE NOT EXISTS (SELECT * FROM game WHERE game.`name`='"+game_name+"') "
-        connector=MysqlConnector()
-        connector.insert(sql=insert_sql)
-        print(game_name+"---------插入成功")
-    print("第"+index.__str__()+"页内容爬取结束")
-    index=index+1
 
+    index=left
+    while index<=right:
+        time.sleep(random.random())
+        myproxies={'http':requests.get(url="http://localhost:5555/random").content.decode()}
+        print("开始爬取第"+index.__str__()+"页内容")
+        x520_url="https://xxxxx520.com/switchyouxi/page/"+index.__str__()
+        pro_headers={"User-Agent":"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.22 Safari/537.36 SE 2.X MetaSr 1.0"}
+        html= requests.get(url=x520_url,headers=pro_headers,proxies=myproxies)
+        soup=BeautifulSoup(html.text,"lxml")
+        res=soup.select("div .placeholder")
+
+        for item in res:
+            tag_a=item.find("a")
+            href=tag_a.attrs['href']
+            inner_html=requests.get(url=href,headers=pro_headers,proxies=myproxies)
+            inner_page=BeautifulSoup(inner_html.text,"lxml")
+            """获取头部"""
+            header=inner_page.find_all("header",class_="entry-header")[0]
+            """获取描述体"""
+            descript_body=inner_page.find_all("div",class_="entry-wrapper")[0]
+            """获取名称，平台,语言属性"""
+            game_name=header.contents[3].text
+            categroy=header.contents[1].contents[3]
+            platform="无"
+            language="无"
+            flag=0;
+            for i in categroy.contents:
+                if i!="\n" and i!="":
+                    if flag==0:
+                        platform=i.text.strip()
+                        flag=1
+                        continue
+                    if flag==1:
+                        language=i.text.strip()
+                        break
+
+                    
+
+            """获取描述"""
+            descript=descript_body.contents[3].text
+
+            """获取下载连接属性"""
+            pattern = re.compile('/[0-9]+')
+            number=pattern.search(href)
+            r_number=number.group()[1:]
+            download_pag_url="https://xxxxx520.com/go/?post_id="+r_number
+            """可能是script结构，也有可能直接是百度链接"""
+            download_pag_script=requests.get(download_pag_url,headers=pro_headers,proxies=myproxies).content.decode()
+            
+            search_res=getDownloadUrl(download_pag_script)
+            if(search_res!=None):
+                download=search_res.group()
+            else :
+                search_res=getfourURL(download_pag_script)
+                """没有链接"""
+                if(search_res==None):
+                    continue
+                download_url=search_res.group()
+                """script结构处理"""
+                download_html=requests.get(download_url,headers=pro_headers,proxies=myproxies)
+                download_pag_soup=BeautifulSoup(download_html.text,"lxml")
+                download=download_pag_soup.find("div",class_="entry-content u-text-format u-clearfix").text
+            
+            
+
+            temp_game=Game(name=game_name,language=language,description=descript,download=download,platefrom=platform)
+            insert_sql="INSERT INTO game  (game.`name`,game.`language`,description,download,plateform) SELECT  '"+game_name+"','"+language+"','"+descript+"','"+download+"','"+platform+"' FROM DUAL   WHERE NOT EXISTS (SELECT * FROM game WHERE game.`name`='"+game_name+"') "
+            connector=MysqlConnector()
+            connector.insert(sql=insert_sql)
+            print(game_name+"---------插入成功")
+        print("第"+index.__str__()+"页内容爬取结束")
+        index=index+1
+
+xxxswitch_crawler()
